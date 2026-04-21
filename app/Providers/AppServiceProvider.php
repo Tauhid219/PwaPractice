d7dfe489f56bcf030c170ee0320f309803189610
Tauhid Ahmad Reza <rezatawhid@gmail.com>
2026-04-21 15:08:29 +0600

    fix: conditionally register TelescopeServiceProvider to prevent production crash

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if ($this->app->environment('local') && class_exists(\App\Providers\TelescopeServiceProvider::class)) {
+            $this->app->register(\App\Providers\TelescopeServiceProvider::class);
+        }
     }
 
     public function boot(): void
