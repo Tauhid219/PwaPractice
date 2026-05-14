11e92f6cd0a13bd0db0e095989b59500de91067d
Tauhid Ahmad Reza <rezatawhid@gmail.com>
2026-05-14 15:18:43 +0600

    Update admin panel branding to Genius Kids

--- resources/views/layouts/admin/footer.blade.php
+++ resources/views/layouts/admin/footer.blade.php
@@ -2,7 +2,7 @@
 <footer class="main-footer">
     <!-- To the right -->
     <div class="float-right d-none d-sm-inline">
-        PwaPractice Admin Panel
+        Genius Kids Admin Panel
     </div>
     <!-- Default to the left -->
     <strong>Copyright &copy; {{ date('Y') }} <a href="{{ route('home') }}">{{ config('app.name', 'Laravel') }}</a>.</strong> All rights reserved.
